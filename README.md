ba2b8a21da59938f8f2b20cf0f51e80ccbad2dfb
Vivek Khimani <vck29@drexel.edu>
2019-10-05 08:50:52 -0400

    v2 completed and uploaded

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # HKUST_Schedule_Crawler_Python
-HKUST (Hong Kong University of Science and Technology) is one of the most popular technical and business institute in Asia and entire world. As a result, all the classes are almost filled and it's not uncommon that students have to spend a lot of time tracking the waitlist quota and schedules on the website. Even I faced the similar problems while deciding on my classes. As a result, I decided to make a web crawler using Python that requires TERM and CLASS as an input and provides all details that a website would provide. Also, I kind of ERROR HANDLING is done in the first version of code itself. Extensive unit testing will be done once the project will be complete. I have already completed more than half of the process in less than 2 hours. I will finish the first version really soon and the next step will be to get in touch with USThing developers and integrate this functionality with their existing app. Thanks! I love doing such fun projects. 
+
+Purpose of the Project:
+General/JustForFun/Personal
+
+Problem Description:
+HKUST (Hong Kong University of Science and Technology) is one of the most popular technical and business institute in Asia and entire world. As a result, all the classes are almost filled and it's not uncommon that students have to spend a lot of time tracking the waitlist quota and schedules on the website. Even I faced the similar problems while deciding on my classes. 
+
+Solution:
+As a result of the above problem, I decided to make a web crawler using Python that requires TERM and CLASS as an input and provides all details that a website would provide. This code can be integrated with any one of the HKUST apps (m.HKUST, HKUSThing) and can be used by the students to check the details of their classes instead of going to the website, finding the class, and manually checking it everytime. 
+
+Error Handling:
+As the code is extremely modular and divided in various functions, extensive ERROR HANDLING and ERROR CATCHING has been done inside the functions itself while taking the inputs from the users. Also, different kind of data in different courses and subjects have been considered and the code is written in such a way that it won't crash in any of those scenarios. Conditional testing and case-wise testing has been done and the screenshots can be found in the 'Screenshots' directory.
+
+Tools Used:
+The crawler has been primarily coded in Python3.6. The libraries included are - BeautifulSoup(v4) for crawling, requests, and validators for fetching and validating the webpage.
+
+Time Required/Versions:
+This is the second version of the project and it's complete with most of the testing done by myself. I will keep on testing and updating the app as and when I get reviews from my fellow classmates and students. This entire version is put together in less than 4 hours.
+
+Next Steps:
+Once I am confident about the bug fixes and get positive reviews from the students, the next step is to demonstrate this concept to one of the HKUST application things. If they like it and find it usable in their respective apps, I would be happy to optimize it according to their needs and provide it to them.
--- source_code/main_crawler.py
+++ source_code/main_crawler.py
@@ -1,11 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import validators
-from datetime import date
 
-#ALREADY TESTED - DO MORE TESTING LATER ON
-#We will get inputs for the TERM and YEAR from the users so that we can fetch the webpage accordingly.
-#Based on the way in which the term pages are stored, we will use it for error handling.
 
 
 def gettingTermInput():
@@ -53,7 +49,7 @@ def gettingTermInput():
 				continue
 
 		plain_text = source_code.text
-		soup_object = BeautifulSoup(plain_text)
+		soup_object = BeautifulSoup(plain_text,"html.parser")
 
 		check_url = "/wcq/cgi-bin/"+str(term_input_split[1][-2:])+str(seasonal_dict.get(term_input_split[0].upper()))+"/"
 		for terms in soup_object.findAll('div',{'class': 'termselect'}):
@@ -67,10 +63,6 @@ def gettingTermInput():
 				break
 
 	
-#print(gettingTermInput())
-
-#ALREADY PRIMARY TESTS DONE. Definitely test more towards the end.
-#Now we will get the inputs for the class of interest once the term is validated. The validated term will provide us a url for the appropriate term. 
 def gettingClassInput(term_url):
 	valid = False
 	class_input = ""
@@ -91,9 +83,9 @@ def gettingClassInput(term_url):
 			print("INVALID SUBJECT SELECTION.")
 
 		appended_class_name = class_split[0]+class_split[1]
-		#print(appended_class_name)
+
 		plain_text = source_code.text
-		soup_object = BeautifulSoup(plain_text)
+		soup_object = BeautifulSoup(plain_text,"html.parser")
 		
 		section_headers = ""
 		for section_headers in soup_object.findAll('a',{'name': appended_class_name}):
@@ -106,21 +98,75 @@ def gettingClassInput(term_url):
 			print("Class Not Found! There might be some error with the COURSE NUMBER. Either it isn't provided this term or doesn't exist at all.\n")
 
 
-#gettingClassInput(gettingTermInput())
-
-#A NEW FUNCTION TO FURTHER PARSE THE DETAILS OF THE CLASS
-#THIS FUNCTION is only executed if the course is available in the required term. We already check that in the previous function.
-
 def getClassDetails(updated_class_url,class_name):
+	course_dict = {}
 	source_code = requests.get(updated_class_url)
 	plain_text = source_code.text
-	soup_object = BeautifulSoup(plain_text)
+	soup_object = BeautifulSoup(plain_text,"html.parser")
 
-	for courses in soup_object.findAll('div',{'class': 'course'}):
-		print(type(courses))
-		#print(str(courses))
-		#print("************")
 
+	sect_head = soup_object.find('a',{'name': class_name})
+	main_course_div = sect_head.parent.parent
+
+	#Item 3 gives course description table
+	#Item 5 gives course header -- DICT DONE
+	#Item 7 gives sections table
+
+	#course_name added to dictionary
+	course_name = str(list(main_course_div.children)[5].string)
+	course_dict['course_name'] = course_name
+
+	#course_info added to dictionary
+	course_info_in_list = list(main_course_div.children)[3].table.findAll('th')
+	course_info_op_list = list(main_course_div.children)[3].table.findAll('td')
+	course_dict['course_info'] = {} 
+	for i in range(len(course_info_in_list)):
+		loc_key = str(course_info_in_list[i].string)
+		course_dict['course_info'][loc_key] = str(course_info_op_list[i].string)
+
+	#course sections added to dictionary
+	sections_details_list = list(main_course_div.children)[7].findAll('td')
+	course_dict['course_sections']={}
+	curr_section = None
+
+	for i in range(len(sections_details_list)):
+		req_str = str(sections_details_list[i].string)
+
+		if (req_str[0] == "L" or req_str[0] == "T") and ((req_str[1] == "A" and req_str[2].isdigit()) or req_str[1].isdigit()):
+			course_dict['course_sections'][req_str] = []
+			curr_section = req_str
+			continue
+
+		else:
+			if len(course_dict['course_sections'][curr_section]) >= 7:
+				continue
+			course_dict['course_sections'][curr_section].append(req_str)
+
+	#returned course_dict
+	#print(course_dict)
+	return course_dict
+
+def printDict(course_dictionary):
+	section_corr_list = ["Day & Time","Room","Instructor","Quota","Enrolled","Avail","Waitlist"]
+	print("**** PLEASE FIND YOUR REQUIRED INFO****\n\n")
+	for vals in course_dictionary.keys():
+		if vals == 'course_name':
+			print("Course Name:",course_dictionary[vals],"\n")
+
+		if vals == 'course_info':
+			for lols in course_dictionary[vals].keys():
+				print(lols,course_dictionary[vals][lols],"\n")
+
+		if vals == 'course_sections':
+			for items in course_dictionary[vals].keys():
+				counter = 0
+				print("\n")
+				print("Section :",items)
+				for more_lols in course_dictionary[vals][items]:
+					print(section_corr_list[counter],":",more_lols,end=" ")
+					counter+=1
 
 url,classN = gettingClassInput(gettingTermInput())
-getClassDetails(url,classN)
+#getClassDetails('https://w5.ab.ust.hk/wcq/cgi-bin/1910/subject/COMP','COMP4900')
+
+printDict(getClassDetails(url,classN))
